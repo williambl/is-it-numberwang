00d2a270e8f171ed034a6a9fed41e3e5497c87fd
williambl <william@williambl.com>
2017-11-01 22:14:46 +0000

    Added a more random element to numberwangification. Fixes #1.

--- numberwang.py
+++ numberwang.py
@@ -1,22 +1,21 @@
 import math
 import random
 
-def determine_numberwang(number):
+def determine_numberwang(number, chance):
     wang_array = [random.randrange(random.randrange(43)), random.randrange(random.randrange(913)), random.randrange(random.randrange(4023))]
     for x in range(1,3):
+        if (random.random() < chance):
+            return;
         if (math.e * wang_array[x-1] ** (1.0 / x) > math.pi * number ** (1.0 / x)):
-            print(math.e * wang_array[x-1] ** (1.0 / x))
-            print(math.pi * number ** (1.0 / x))
             if (math.sin(math.pi * wang_array[x-1] ** (1.0 / x)) > math.cos(math.e * number ** (1.0 / x))):
-                print(math.sin(math.pi * wang_array[x-1] ** (1.0 / x))) 
-                print(math.cos(math.e * number ** (1.0 / x)))
-                if (random.random() > 5):
+                if (random.random() > 6):
                     if (1/e**wang_array[x-1] > 1/number):
                         print("That's Numberwang!")
                         return;
                 else:
-                    print("That's Numberwang!")
+                    return;
 
 
-while (True):
-    determine_numberwang(float(input("> ")))
+for i in range(1,3):
+    for j in range(10):
+        determine_numberwang(float(input("> ")), 1/(i*2))
