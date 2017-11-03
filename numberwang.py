013cf5af1dfc0ab3436b452a21f6e75f3684b852
williambl <william@williambl.com>
2017-11-03 22:40:51 +0000

    Added a point system

--- numberwang.py
+++ numberwang.py
@@ -16,10 +16,13 @@ def determine_numberwang(number, chance):
         return False;
 
 if (__name__ == "__main__"):
+    points = 0;
+
     for i in range(1,4): # Three rounds
         print("Round " + str(i) + ":")
         for j in range(0,5): # 5 chances
             if (determine_numberwang(float(input("> ")), 1.0/(i*2))):
+                points += 1
                 print("That's Numberwang!")
             else:
                 print("That's not Numberwang.")
@@ -29,6 +32,8 @@ if (__name__ == "__main__"):
         print("Round " + str(i) + ":")
         for j in range(0,5): # 5 chances
             if (determine_numberwang(float(input("> ")), 1.0/(i*2))):
+                poits += 1
                 print("That's WangerNumb!")
             else:
                 print("That's not WangerNumb.")
+    print("\nYou got " + str(points) + " points!")
