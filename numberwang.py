44c25334a089ca7528e34d6388215926f8a6b044
williambl <william@williambl.com>
2017-11-05 21:18:42 +0000

    Removed the mathematics - it's better just being random.

--- numberwang.py
+++ numberwang.py
@@ -1,19 +1,11 @@
-import math
 import random
 
 def determine_numberwang(number, chance):
-    wang_array = [random.randrange(random.randrange(43)), random.randrange(random.randrange(913)), random.randrange(random.randrange(4023))]
     for x in range(1,3):
         if (random.random() < chance):
             return True;
-        if (math.e * wang_array[x-1] ** (1.0 / x) > math.pi * number ** (1.0 / x)):
-            if (math.sin(math.pi * wang_array[x-1] ** (1.0 / x)) > math.cos(math.e * number ** (1.0 / x))):
-                if (random.random() > 6):
-                    if (1/e**wang_array[x-1] > 1/number):
-                        return True;
-                else:
-                    return True;
-        return False;
+        else:
+            return False;
 
 if (__name__ == "__main__"):
     points = 0;
