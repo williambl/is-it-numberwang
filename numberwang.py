3e1fc467ad8e9c256aa5f92e25582dae1116fa74
williambl <william@williambl.com>
2017-11-07 21:28:18 +0000

    Moved populating numberwang array to a function

--- numberwang.py
+++ numberwang.py
@@ -7,20 +7,26 @@ def determine_numberwang(number, chance):
         else:
             return False;
 
+def populate_numberwang_array():
+    array = []
+    for i in range(random.randrange(10,20)):
+        # add between 10 and 20 values to the numberwang array
+        if (random.random() < 0.3):
+            array += [float(random.randrange(-1020,5000)/100)]
+        else:
+            array += [random.randrange(-102,500)]
+    return array
+
 if (__name__ == "__main__"):
     points = 0;
     numberwang_array = []
 
-    for i in range(random.randrange(10,20)):
-        # add between 10 and 20 values to the numberwang array
-        if (random.random() < 0.3):
-            numberwang_array += [float(random.randrange(-1020,5000)/100)]
-        else:
-            numberwang_array += [random.randrange(-102,500)]
-
     for i in range(1,4): # Three rounds
         print("Round " + str(i) + ":")
+
+        numberwang_array = populate_numberwang_array()
         print(numberwang_array)
+
         for j in range(0,5): # 5 chances
             if (determine_numberwang(float(input("> ")), 1.0/(i*2))):
                 points += 1
@@ -31,6 +37,10 @@ if (__name__ == "__main__"):
     print("\nLet's Rotate the Board! It's time for WangerNumb!\n")
     for i in range(1,4): # Three rounds
         print("Round " + str(i) + ":")
+
+        numberwang_array = populate_numberwang_array()
+        print(numberwang_array)
+
         for j in range(0,5): # 5 chances
             if (determine_numberwang(float(input("> ")), 1.0/(i*2))):
                 points += 1
